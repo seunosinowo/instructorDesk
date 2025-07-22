f3748769bc8d0ea22b317ec349abe788e5c9aabe
seunosinowo <oluwaseunpaul98@gmail.com>
2025-07-22 14:23:12 +0100

    Modified and error logs

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,4 +1,12 @@
+import { constrainedMemory } from 'process';
 import { Sequelize } from 'sequelize';
+
+// console.log('DB_NAME:', process.env.DB_NAME);
+// console.log('DB_USER:', process.env.DB_USER);
+// console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+// console.log('DB_HOST:', process.env.DB_HOST);
+// console.log('DB_PORT:', process.env.DB_PORT);
+// console.log('CLOUDINARY_CLOUD_NAME:', process.env.CLOUDINARY_CLOUD_NAME)
 
 const sequelize = new Sequelize(
   process.env.DB_NAME!,
