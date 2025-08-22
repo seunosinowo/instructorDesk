4899c9baf5933cc906e234870006ea2d064a031f
seunosinowo <oluwaseunpaul98@gmail.com>
2025-08-22 10:56:35 +0100

    fix: correct path in copy-email-template.js to copy from backend root to dist folder

--- backend/copy-email-template.js
+++ backend/copy-email-template.js
@@ -2,8 +2,9 @@
 const fs = require('fs');
 const path = require('path');
 
-const src = path.join(__dirname, '../emailTemplate.html');
-const dest = path.join(__dirname, './dist/emailTemplate.html');
+
+const src = path.join(__dirname, 'emailTemplate.html');
+const dest = path.join(__dirname, 'dist', 'emailTemplate.html');
 
 fs.copyFileSync(src, dest);
 console.log('Copied emailTemplate.html to dist folder.');
